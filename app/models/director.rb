e0672c08f88cd67d4d1ef4e93512beacd29e26f8
Ben Purinton <ben.purinton@gmail.com>
2022-12-09 03:38:02 +0000

    copy paste association accessors from refact msm 2

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,13 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  has_many(:filmography, { :class_name => "Movie" , :foreign_key => "director_id" })
+
+  # def filmography
+  #   key = self.id
+
+  #   the_many = Movie.where({ :director_id => key })
+
+  #   return the_many
+  # end
 end
